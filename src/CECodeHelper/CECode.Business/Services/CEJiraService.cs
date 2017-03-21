8e2af8cf2a231f904f08eba0961fe04eaf9c8e6d
RobRobertsCE <rroberts@centeredgesoftware.com>
2017-03-20 22:42:07 -0400

    updated adapter references

--- src/CECodeHelper/CECode.Business/Services/CEJiraService.cs
+++ src/CECodeHelper/CECode.Business/Services/CEJiraService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CECode.Business.Adapters;
 using CECode.Jira.Service;
 
 namespace CECode.Business.Services
